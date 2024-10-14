00ce2766ab542550d0dc8e4fb4e28c7c18b6c400
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-10-14 12:08:03 -0700

    Requisitos 4 e 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import parsel
+from .database import create_news
 
 
 def fetch(url):
@@ -26,7 +27,7 @@ def scrape_updates(html_content):
     return news_urls if news_urls else []
 
 
-def scrape_next_page_link(html_content):
+def scrape_next_page(html_content):
     selector = parsel.Selector(html_content)
 
     next_page = selector.css("a.next.page-numbers::attr(href)").get()
@@ -36,13 +37,60 @@ def scrape_next_page_link(html_content):
     return None
 
 
-# Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = parsel.Selector(html_content)
+
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css('h1.entry-title::text').get().strip()
+    timestamp = selector.css('li.meta-date::text').get()
+    writer = selector.css('span.author a::text').get()
+
+    reading_time = int(
+        selector.css('li.meta-reading-time::text').re_first(r'\d+')
+        )
+
+    summary = "".join(selector.css(
+        'div.entry-content > p:first-of-type *::text'
+        ).getall()).strip()
+
+    category = selector.css('a.category-style span.label::text').get()
+
+    news_data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
+
+    return news_data
 
 
-# Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    url = 'https://blog.betrybe.com/'
+    news_list = []
+
+    while len(news_list) < amount:
+        html_content = fetch(url)
+
+        news_urls = scrape_updates(html_content)
+
+        for news_url in news_urls:
+            if len(news_list) >= amount:
+                break
+            news_html = fetch(news_url)
+            news_data = scrape_news(news_html)
+            news_list.append(news_data)
+
+        url = scrape_next_page(html_content)
+
+        if not url:
+            break
+
+        time.sleep(1)
+
+    create_news(news_list)
+
+    return news_list
