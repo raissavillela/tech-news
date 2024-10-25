20b1319288f7fcc2e3a73bc333db0d680b2212fb
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-10-25 09:19:01 -0700

    Feat.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,30 @@
-# Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+from tech_news.database import db
+from datetime import datetime
 
 
-# Requisito 8
-def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+def search_by_title(title: str):
+
+    criteria = {"title": {"$regex": title, "$options": "i"}}
+    news_list = db.news.find(criteria)
+
+    return [(news["title"], news["url"]) for news in news_list]
 
 
-# Requisito 9
-def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+def search_by_date(date: str):
+    try:
+        iso_date = datetime.strptime(date, "%Y-%m-%d")
+        formatted_date = iso_date.strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    criteria = {"timestamp": formatted_date}
+    news_list = db.news.find(criteria)
+
+    return [(news["title"], news["url"]) for news in news_list]
+
+
+def search_by_category(category: str):
+    criteria = {"category": {"$regex": category, "$options": "i"}}
+    news_list = db.news.find(criteria)
+
+    return [(news["title"], news["url"]) for news in news_list]
--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,40 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+import pytest
+from unittest.mock import patch
+from tech_news.analyzer.reading_plan import ReadingPlanService
+
+
+def mock_find_news():
+    return [
+        {"title": "Notícia 1", "reading_time": 4},
+        {"title": "Notícia 2", "reading_time": 3},
+        {"title": "Notícia 3", "reading_time": 10},
+        {"title": "Notícia 4", "reading_time": 15},
+        {"title": "Notícia 5", "reading_time": 12},
+    ]
 
 
 def test_reading_plan_group_news():
-    pass
+    with pytest.raises(
+       ValueError, match="Valor 'available_time' deve ser maior que zero"):
+        ReadingPlanService.group_news_for_available_time(0)
+
+    with patch('tech_news.analyzer.reading_plan.find_news', mock_find_news):
+        result = ReadingPlanService.group_news_for_available_time(10)
+
+        expected_readable = [
+            {
+                "unfilled_time": 3,
+                "chosen_news": [("Notícia 1", 4), ("Notícia 2", 3)]
+            },
+            {
+                "unfilled_time": 0,
+                "chosen_news": [("Notícia 3", 10)]
+            }
+        ]
+        assert result["readable"] == expected_readable
+
+        expected_unreadable = [
+            ("Notícia 4", 15),
+            ("Notícia 5", 12)
+        ]
+        assert result["unreadable"] == expected_unreadable
